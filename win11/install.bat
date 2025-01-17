efdd01b46c30693fb52327be4b63316e1d944551
holychowders <theholychowders@gmail.com>
2025-01-17 02:16:03 -0500

    Update install.bat

--- win11/install.bat
+++ win11/install.bat
@@ -1,8 +1,9 @@
 @echo off
 
+mklink C:\Users\holychowders\.glzr\glazewm\config.yaml C:\Users\holychowders\docs\cs\WinuxDotfiles\win11\home\.glzr\glazewm\config.yaml
 mklink C:\Users\holychowders\.vimrc C:\Users\holychowders\docs\cs\WinuxDotfiles\common\home\.vimrc
 mklink C:\Users\holychowders\.gitconfig C:\Users\holychowders\docs\cs\WinuxDotfiles\common\home\.gitconfig
-mklink C:\Users\holychowders\.glzr\glazewm\config.yaml C:\Users\holychowders\docs\cs\WinuxDotfiles\win11\home\.glzr\glazewm\config.yaml
+mklink C:\Users\holychowders\themes.gitconfig C:\Users\holychowders\docs\cs\WinuxDotfiles\common\home\themes.gitconfig
 
 REM Install some things
 REM TODO: See if we can use winget to install GlazeWM, Git, Visual Studio (and install the Desktop C++ component), and the other apps we want.
