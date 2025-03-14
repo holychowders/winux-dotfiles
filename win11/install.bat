5f47b3a757a63eac5a34d74a75386298d58dfc52
holychowders <theholychowders@gmail.com>
2025-03-14 18:21:30 -0400

    Note some desired applications to add to installer

--- win11/install.bat
+++ win11/install.bat
@@ -14,6 +14,21 @@ mklink C:\Users\holychowders\.gitconfig C:\Users\holychowders\docs\cs\WinuxDotfi
 mklink C:\Users\holychowders\themes.gitconfig C:\Users\holychowders\docs\cs\WinuxDotfiles\common\home\themes.gitconfig
 
 REM Install some things
+REM All applications desired:
+REM Steam
+REM Discord
+REM Firefox (TODO: new browser)
+REM RemedyBG
+REM ProcMon
+REM Spy++ (included with Visual Studio as a standalone application)
+REM VMWare Workstation
+REM Vim
+REM ImHex
+REM iato
+REM CheatEngine
+REM x64Dbg
+REM Notepad++
+
 REM winget install vim.vim
 winget install nvim
 winget install dandavison.delta
