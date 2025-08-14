a2c8288e8d9046b1caabf30dad9a9da28b0b4fcd
holychowders <theholychowders@gmail.com>
2025-08-13 21:35:48 -0700

    start reworking Windows config

--- win11/README.md
+++ win11/README.md
@@ -1,3 +1,27 @@
-# README
+# Winux: Windows 11 Dotfiles
 
-To install: Run `install.bat` as administrator from any directory.
+**REWORK IN PROGRESS: USE WITH CAUTION**
+
+## Installation
+
+After installing and logging on to Windows...
+
+`install.bat` as admin...
+
+### Todo (documentation)
+
+- Set background
+- Windows Update
+- Applications: VMWare Workstation, RemedyBG
+- Windows Terminal configuration file
+- Verify thorough removal of addon antivirus
+- O&O Shutup10++ usage
+- Visual Studio Community setup
+- Firefox setup
+
+## Resources
+
+- Red Hat Mono font: <https://github.com/RedHatOfficial/RedHatFont>
+- FiraCode font: <https://github.com/tonsky/FiraCode>
+- Windows Terminal Themes: <https://windowsterminalthemes.dev> (<https://github.com/atomcorp/themes>)
+- The `gruvbox-baby` themes used throughout my configuration are derived from <https://github.com/luisiacc/gruvbox-baby>
--- win11/install.bat
+++ win11/install.bat
@@ -1,44 +1,86 @@
 @echo off
 
-REM Todos
-REM TODO: See if we can use winget to install GlazeWM, Git, Visual Studio (and install the Desktop C++ component), and the other apps we want.
-REM FIXME: Fix vim installation. Use nvim for now.
+REM TODO:
+REM   FIXME: Fix Vim installation. Use Nvim for now.
 
-REM Symlink configs
-mklink C:\Users\holychowders\.glzr\glazewm\config.yaml C:\Users\holychowders\docs\cs\winux-dotfiles\win11\home\.glzr\glazewm\config.yaml
-REM FIXME
-REM mklink C:\Users\holychowders\.vimrc C:\Users\holychowders\docs\cs\winux-dotfiles\common\home\.vimrc
-mkdir C:\Users\holychowders\AppData\Local\nvim
-mklink C:\Users\holychowders\AppData\Local\nvim\init.vim C:\Users\holychowders\docs\cs\winux-dotfiles\common\home\.vimrc
-mklink C:\Users\holychowders\.gitconfig C:\Users\holychowders\docs\cs\winux-dotfiles\common\home\.gitconfig
-mklink C:\Users\holychowders\themes.gitconfig C:\Users\holychowders\docs\cs\winux-dotfiles\common\home\themes.gitconfig
+REM REWORK
+echo **REWORK IN PROGRESS: USE WITH CAUTION**
+echo **Exiting**
+exit /B 1
 
-REM Install some things
-REM All applications desired:
-REM Git for Windows (will get linux tools and make available on windows)
-REM WSL
-REM ctags
-REM git delta
-REM bed ("binary editor written in go")
-REM WinDbg
-REM Rust (rustup)
-REM Steam
-REM Discord
-REM PowerToys
-REM Firefox (TODO: new browser)
-REM RemedyBG
-REM ProcMon
-REM Spy++ (included with Visual Studio as a standalone application)
-REM VMWare Workstation
-REM Vim
-REM ImHex
-REM iato
-REM CheatEngine
-REM x64Dbg
-REM Notepad++
+REM SWITCH SYSTEM-WIDE THEME TO DARK MODE
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
+taskkill /f /im explorer.exe && start explorer.exe
 
+REM MAKE REQUIRED DIRECTORIES (AND UPDATE PATH)
+mkdir docs\cs
+
+mkdir docs\bin
+set "NEW_PATH=%PATH%;%USERPROFILE%\docs\bin"
+setx /M PATH "%NEW_PATH%"
+
+
+REM SYMLINK CONFIGURATION FILES
+mklink %USERPROFILE%\.glzr\glazewm\config.yaml %USERPROFILE%\docs\cs\winux-dotfiles\win11\home\.glzr\glazewm\config.yaml
+REM mklink %USERPROFILE%\.vimrc %USERPROFILE%\docs\cs\winux-dotfiles\common\home\.vimrc
+mkdir %LOCALAPPDATA%\nvim
+mklink %LOCALAPPDATA%\nvim\init.vim %USERPROFILE%\docs\cs\winux-dotfiles\common\home\.vimrc
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\docs\cs\winux-dotfiles\common\home\.gitconfig
+mklink %USERPROFILE%\themes.gitconfig %USERPROFILE%\docs\cs\winux-dotfiles\common\home\themes.gitconfig
+
+REM INSTALL APPLICATIONS
+winget install OO-Software.ShutUp10
+winget install Microsoft.VisualStudio.2022.Community
 winget install vim.vim
-winget install nvim
+winget install Neovim.Neovim
+winget install Notepad++.Notepad++
+winget install Microsoft.Sysinternals.Suite
+winget install Microsoft.PowerToys
+winget install Git.Git
 winget install dandavison.delta
-winget install ctags
-wsl --install
+winget install Mozilla.Firefox
+
+winget install glzr-io.glazewm
+winget install TheDocumentFoundation.LibreOffice
+winget install Valve.Steam
+winget install FxSound.FxSound
+winget install Discord.Discord
+winget install OBSProject.OBSStudio
+winget install VideoLAN.VLC
+winget install Microsoft.WinDbg
+winget install WerWolv.ImHex
+winget install UniversalCtags.Ctags
+winget install Gitleaks.Gitleaks
+
+winget install LLVM.LLVM
+winget install Python.Python.3.13
+winget install GoLang.Go
+winget install Rustlang.Rustup
+wsl --install -d Ubuntu
+winget install Docker.DockerDesktop
+winget install Kubernetes.kind
+
+rem bed (Binary editor written in Go)
+curl -L "https://github.com/itchyny/bed/releases/download/v0.2.8/bed_v0.2.8_windows_amd64.zip" -o "bed_v0.2.8_windows_amd64.zip"
+tar -xf "bed_v0.2.8_windows_amd64.zip"
+copy "bed_v0.2.8_windows_amd64\bed.exe" "docs\bin"
+
+
+REM INSTALL FONTS
+
+REM Red Hat Mono
+curl -L "https://github.com/RedHatOfficial/RedHatFont/archive/refs/tags/4.1.0.zip" -o "RedHatFont-4.1.0.zip"
+tar -xf "RedHatFont-4.1.0.zip"
+copy "RedHatFont-4.1.0\fonts\Mono\ttf\" "%WINDIR%/fonts"
+
+rem FiraCode
+curl -L "https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip" -o "Fira_Code_v6.2.zip"
+tar -xf "Fira_Code_v6.2.zip" -C "Fira_Code_v6.2"
+copy "Fira_Code_v6.2\ttf\" "%WINDIR%/fonts"
+
+
+REM WINDOWS TERMINAL THEMES
+curl -L "https://github.com/atomcorp/themes/blob/master/themes.json" -o "windows-terminal-themes.json"
+curl -L "https://github.com/luisiacc/gruvbox-baby/blob/main/extras/windwows_terminal/windows_terminal_dark.json" -o "gruvbox-baby-dark.json"
+
